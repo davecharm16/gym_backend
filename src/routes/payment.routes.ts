8ef3e6c989333703b04157d0aaff56d87656bf21
d_francisgonzales <francisgonzales.dev@gmail.com>
2025-06-23 01:12:50 +0800

    fix: routing problems

--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -13,7 +13,7 @@ import {
 const router = Router();
 
 router.post('/', requireAuth, createPayment); // Create a payment
-router.get('/:studentId', requireAuth, getPaymentsByStudent); // Get payments for a student
+router.get('/student/:studentId', requireAuth, getPaymentsByStudent); // Get payments for a student
 router.get('/all-with-student', requireAuth, getAllPaymentsWithStudent); // Get payments for a student
 router.get('/average', requireAuth, getPaymentAverages); // Get payments for a student
 router.put('/:id', requireAuth, updatePayment); // Update a payment
